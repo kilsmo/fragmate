26d1e60fbf333b230e11fe3f3a7c0affff6be1aa
Nicklas Westin <kilsmo@gmail.com>
2020-11-25 11:01:13 +0100

    Added code documentation for the functions init, render, and elm

--- src/elm.js
+++ src/elm.js
@@ -1,3 +1,14 @@
+/**
+ * Returns an element with a name and its props.
+ * children is a special prop, to simplify the
+ * api. In children, the children of this elm
+ * is placed. If a child is a string, a TextNode
+ * will be created, otherwise the child node will
+ * be returned.
+ * 
+ * @param {string} name
+ * @param {object} props
+ */
 export function elm(name, props) {
   const e = document.createElement(name);
   for (let x in props) {
--- src/fragmate.js
+++ src/fragmate.js
@@ -1,11 +1,23 @@
 let parentElm;
 let renderFn;
 
+/**
+ * Initialize fragmate a parent element, where render will happen,
+ * and a render function, that will be used in render.
+ * 
+ * @param {HTMLElement} parentElement 
+ * @param {function} renderFunction 
+ */
 export function init(parentElement, renderFunction) {
   parentElm = parentElement;
   renderFn = renderFunction;
 }
 
+/**
+ * Renders the state, with the function that was set with init.
+ * 
+ * @param {object} state The state to render. (an object)
+ */
 export function render(state) {
   const renderTree = renderFn(state);
   parentElm.innerHTML = '';
